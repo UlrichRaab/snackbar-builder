020734562a682da108acbf37c79b7d7570fecef9
Ulrich Raab <ulrich.raab@ti8m.ch>
2016-06-16 16:11:38 +0200

    Updated demo app

--- app/src/main/java/de/ulrichraab/snackbar/app/MainActivity.java
+++ app/src/main/java/de/ulrichraab/snackbar/app/MainActivity.java
@@ -1,8 +1,13 @@
 package de.ulrichraab.snackbar.app;
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import de.ulrichraab.snackbar.SnackbarBuilder;
+import de.ulrichraab.snackbar.SnackbarStyle;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
    protected void onCreate (Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+   }
+
+   @Override
+   protected void onResume () {
+      super.onResume();
+      View snackbarContainer = findViewById(android.R.id.content);
+      SnackbarStyle errorStyle = createErrorSnackbarStyle();
+      new SnackbarBuilder().container(snackbarContainer)
+                           .message("test")
+                           .style(errorStyle)
+                           .duration(Snackbar.LENGTH_INDEFINITE)
+                           .build()
+                           .show();
+   }
+
+
+   private SnackbarStyle createErrorSnackbarStyle () {
+      return new SnackbarStyle(R.color.red, R.color.red, R.color.white);
    }
 }
